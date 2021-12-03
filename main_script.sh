992c9a95cf3563875c5377d413663892af188d9d
Nuolon <Dryxonin@gmail.com>
2021-12-03 11:20:33 +0100

    fixed v2

--- main_script.sh
+++ main_script.sh
@@ -27,6 +27,7 @@ roll() {
 }
 
 start() {
+echo -e "${YEL}"
 echo -e "${BLINKPURP}###${NC} ${RED}Welcome to${NC} ${LPURPLE}Nick's${NC} ${RED}httpd/apache webserver roll-out script${NC}${BLINKPURP} ###${NC}"
 echo -e "${CYAN}Please make sure you run this script as${NC}${RED} privileged user${NC}${CYAN}, are you?${NC}${YEL} [Y/N] ${NC}"
 read -p "Input: " -n 1 -r
@@ -39,6 +40,7 @@ fi
 }
 
 selinux_passive() {
+echo -e "${YEL}"
 roll "Configuring selinux..."
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
@@ -48,6 +50,7 @@ echo -e "${LPURPLE}========================================================${YEL
 }
 
 change_hostname() {
+echo -e "${YEL}"
 roll "Changing hostname to: G05-Web01"
 hostnamectl set-hostname G05-Web01
 echo -e "${YEL}"
@@ -56,6 +59,7 @@ echo -e "${LPURPLE}========================================================${YEL
 }
 
 installing_dnf_utils() {
+echo -e "${YEL}"
 dnf install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
 dnf module enable -y php:remi-7.4
 echo -e "${YEL}"
@@ -64,6 +68,7 @@ echo -e "${LPURPLE}========================================================${YEL
 }
 
 install_httpd_apache() {
+echo -e "${YEL}"
 roll "Starting installation of \"httpd\" (apache) webserver via dnf..."
 dnf -y install httpd
 echo -e "${YEL}"
@@ -71,6 +76,7 @@ roll "Done!"
 echo -e "${LPURPLE}========================================================${YEL}"
 }
 install_PHPreqsForHttpd() {
+echo -e "${YEL}"
 roll "Starting download of PHP and good-to-have modules..."
 dnf -y install php php-common php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-gd php-mbstring php-xml php-json php-pecl-zip libzip php-intl
 roll "Restarting Apache/httpd and configuring a PHP test site..."
@@ -84,6 +90,7 @@ echo -e "${LPURPLE}========================================================${YEL
 }
 
 start_and_enable_httpd_systemctl() {
+echo -e "${YEL}"
 roll "Starting httpd and enabling it to run on boot..."
 systemctl enable httpd
 systemctl start httpd
@@ -93,6 +100,7 @@ echo -e "${LPURPLE}========================================================${YEL
 }
 
 download_install_permissions_for_moodle() {
+echo -e "${YEL}"
 wget -c https://download.moodle.org/download.php/direct/stable311/moodle-3.11.4.tgz
 tar -xzf moodle-3.11.4.tgz -C /var/www/
 chmod 775 -R /var/www/moodle
@@ -108,7 +116,7 @@ echo -e "${LPURPLE}========================================================${YEL
 
 
 configure_moodle() {
-
+echo -e "${YEL}"
 #cp /var/www/html/moodle/config-dist.php /var/www/html/moodle/config.php
 sed -i 's\$CFG->dbtype    = '\''pgsql'\'';\$CFG->dbtype    = '\''mariadb'\'';\' /var/www/html/moodle/config.php
 sed -i 's\$CFG->dbname    = '\''moodle'\'';\$CFG->dbname    = '\''moodledb'\'';\' /var/www/html/moodle/config.php
@@ -142,6 +150,7 @@ echo -e "${LPURPLE}========================================================${YEL
 }
 
 configure_firewall() {
+echo -e "${YEL}"
 roll "Configuring firewall settings..."
 firewall-cmd --add-port=80/tcp --zone=public --permanent
 firewall-cmd --add-port=443/tcp --zone=public --permanent
